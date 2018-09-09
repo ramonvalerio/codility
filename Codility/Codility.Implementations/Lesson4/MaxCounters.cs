0a7a1b2973a6506b4d1a63bd525e786e6cb55c3f
Ramon Valerio <ramonvalerios@gmail.com>
2018-09-09 16:29:52 -0300

    incomplete solution MaxCounters

---
+++ Codility/Codility.Implementations/Lesson4/MaxCounters.cs
@@ -0,0 +1,25 @@
+﻿using System.Linq;
+
+namespace Codility.Implementations.Lesson4
+{
+    public class MaxCounters
+    {
+        public int[] solution(int N, int[] A)
+        {
+            int[] counters = Enumerable.Repeat(0, N).ToArray();
+
+            for (int x = 0; x < N; x++)
+            {
+                for (int k = 0; k < A.Length; k++)
+                {
+                    if (A[k] == x && 1 <= x && x <= N)
+                    {
+                        counters[x] = counters[x] + 1;
+                    }
+                }
+            }
+
+            return counters;
+        }
+    }
+}
---
+++ Codility/Codility.Test/Lesson4/MaxCountersTest.cs
@@ -0,0 +1,24 @@
+﻿using Codility.Implementations.Lesson4;
+using Xunit;
+
+namespace Codility.Test.Lesson4
+{
+    public class MaxCountersTest
+    {
+        [Fact]
+        public void Case01()
+        {
+            // Arrange
+            var algorithm = new MaxCounters();
+            int N = 5;
+            var A = new int[] { 3, 4, 4, 6, 1, 4, 4 };
+
+            // Act
+            var result = algorithm.solution(N, A);
+            var expected = new int[]{ 3, 2, 2, 4, 2 };
+
+            // Assert
+            Assert.Equal(expected, result);
+        }
+    }
+}
