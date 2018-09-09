8b1fa130c6a0b210386326b6262a60fa7402924c
Ramon Valerio <ramonvalerios@gmail.com>
2018-09-09 14:49:38 -0300

    PassingCars
    First implementation

---
+++ Codility/Codility.Implementations/Lesson5/PassingCars.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Codility.Implementations.Lesson5
+{
+    public class PassingCars
+    {
+        public int solution(int[] A)
+        {
+            const int east = 0;
+            const int west = 1;
+
+            var carsEast = new HashSet<int>();
+
+            for (int i = 0; i < A.Length; i++)
+            {
+                if (A[i] == east)
+                    carsEast.Add(i);
+            }
+
+            var result = new List<Tuple<int, int>>();
+
+            foreach (var carEast in carsEast)
+            {
+                for(int i = 0; i < A.Length; i++)
+                {
+                    if (carEast < i && A[i] == west)
+                        result.Add(new Tuple<int, int>(carEast, i));
+                }
+            }
+
+            var limitCars = 1000000000;
+
+            return (result.Count > limitCars? -1 : result.Count);
+        }
+
+        public int solution2(int[] A)
+        {
+
+
+            return 0;
+        }
+    }
+}
---
+++ Codility/Codility.Test/Lesson5/PassingCarsTest.cs
@@ -0,0 +1,24 @@
+﻿using Codility.Implementations.Lesson5;
+using Xunit;
+
+namespace Codility.Test.Lesson5
+{
+    public class PassingCarsTest
+    {
+        [Fact]
+        public void Case01()
+        {
+            // Arrange
+            var A = new int[] {0, 1, 0, 1, 1};
+            var algorithm = new PassingCars();
+
+            // Act
+            int result = algorithm.solution(A);
+
+            // Assert
+            int expected = 5;
+            Assert.Equal(expected, result);
+
+        }
+    }
+}
