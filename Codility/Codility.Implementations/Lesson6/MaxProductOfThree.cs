b9657c4bde96aa0e76c660ab403e15bf1721526b
Ramon Valerio <ramonvalerios@gmail.com>
2018-09-17 04:54:35 -0300

    solution of MaxProductOfThree in progress

---
+++ Codility/Codility.Implementations/Lesson6/MaxProductOfThree.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+namespace Codility.Implementations.Lesson6
+{
+    public class MaxProductOfThree
+    {
+        public int solution(int[] A)
+        {
+            if (A.Length < 3)
+                throw new Exception("Invalid argument");
+
+            Array.Sort(A);
+
+            return A.Max();
+        }
+    }
+}
---
+++ Codility/Codility.Test/Lesson6/MaxProductOfThreeTest.cs
@@ -0,0 +1,23 @@
+﻿using Codility.Implementations.Lesson6;
+using Xunit;
+
+namespace Codility.Test.Lesson6
+{
+    public class MaxProductOfThreeTest
+    {
+        [Fact]
+        public void Case01()
+        {
+            // Arrange
+            var algorithm = new MaxProductOfThree();
+            var A = new int[] { -3, 1, 2, -2, 5, 6 };
+
+            // Act
+            var result = algorithm.solution(A);
+            var expected = 60;
+
+            // Assert
+            Assert.Equal(expected, result);
+        }
+    }
+}
