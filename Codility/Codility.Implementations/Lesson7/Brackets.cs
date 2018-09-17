304e28d0aa2cc1fc3dacfd9c1ec7d65596d797c2
Ramon Valerio <rvalerio@tecgraf.puc-rio.br>
2018-09-17 20:06:45 -0300

    Brackets

---
+++ Codility/Codility.Implementations/Lesson7/Brackets.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections.Generic;
+
+namespace Codility.Implementations.Lesson7
+{
+    public class Brackets
+    {
+        public int solution(string S)
+        {
+            var pares = new Dictionary<char, char>();
+            pares.Add('}', '{');
+            pares.Add(']', '[');
+            pares.Add(')', '(');
+
+            var pilha = new Stack<char>();
+
+            for (int i = 0; i < S.Length; i++)
+            {
+                if (pares.ContainsKey(S[i]))
+                {
+                    if (pilha.Count == 0)
+                        return 0;
+
+                    if (pilha.Pop() != pares[S[i]])
+                        return 0;
+                }
+                else
+                {
+                    pilha.Push(S[i]);
+                }
+            }
+
+            return pilha.Count == 0 ? 1 : 0;
+        }
+
+        public int solution2(string S)
+        {
+            if (string.IsNullOrWhiteSpace(S))
+                return 1;
+
+            var pares = new Dictionary<char, char>();
+            pares.Add('}', '{');
+            pares.Add(']', '[');
+            pares.Add(')', '(');
+
+            var pilha = new Stack<char>();
+
+            for (int i = 0; i < S.Length; i++)
+            {
+                if (pares.ContainsKey(S[i]))
+                {
+                    if (pilha.Count == 0)
+                        return 0;
+
+                    if (pilha.Pop() != pares[S[i]])
+                    {
+                        return 0;
+                    }
+                }
+                else
+                {
+                    pilha.Push(S[i]);
+                }
+            }
+
+            return 1;
+        }
+    }
+}
---
+++ Codility/Codility.Test/Lesson7/BracketsTest.cs
@@ -0,0 +1,43 @@
+﻿using Codility.Implementations.Lesson7;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Codility.Test.Lesson7
+{
+    public class BracketsTest
+    {
+        [Fact]
+        public void Case1()
+        {
+            // Arrange
+            var S = "{[()()]}";
+            var aaa = new Brackets();
+
+            // Act
+            var result = aaa.solution(S);
+
+            // Assert
+            var expected = 1;
+
+            Assert.Equal(expected, result);
+        }
+
+        [Fact]
+        public void Case2()
+        {
+            // Arrange
+            var S = "{[()([]}";
+            var aaa = new Brackets();
+
+            // Act
+            var result = aaa.solution(S);
+
+            // Assert
+            var expected = 0;
+
+            Assert.Equal(expected, result);
+        }
+    }
+}
