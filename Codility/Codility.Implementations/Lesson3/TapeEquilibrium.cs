7774ab77e151e4f3e26d65e5bd3209ccbf158234
Ramon Valerio <ramonvalerios@gmail.com>
2018-09-16 22:37:32 -0300

    TapeEquilibrium

---
+++ Codility/Codility.Implementations/Lesson3/TapeEquilibrium.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Codility.Implementations.Lesson3
+{
+    public class TapeEquilibrium
+    {
+        public int solution(int[] A)
+        {
+            var result = int.MaxValue;
+            var upperPart = 0;
+            var lowerPart = A.Sum();
+
+            for (int i = 1; i < A.Length; i++)
+            {
+                upperPart += A[i - 1];
+                lowerPart -= A[i - 1];
+
+                var currentDifference = Math.Abs(upperPart - lowerPart);
+
+                if (currentDifference < result)
+                    result = currentDifference;
+            }
+
+            return result;
+        }
+
+        public int solution2(int[] A)
+        {
+            var places = new SortedSet<int>();
+
+            int currentIndex = 0;
+            int firstValue = 0;
+            int accum = 0;
+
+            int total = A.Length;
+            int i = 0;
+
+            while (i < total && currentIndex < total - 1)
+            {
+                if (i == currentIndex)
+                    firstValue += A[i];
+                else if (i > currentIndex)
+                    accum += A[i];
+
+                if (i == total - 1)
+                {
+                    places.Add(Math.Abs(firstValue - accum));
+                    currentIndex++;
+                    i = currentIndex;
+                    accum = 0;
+                }
+                else
+                    i++;
+            }
+
+            return places.Min();
+        }
+    }
+}
---
+++ Codility/Codility.Test/Lesson3/TapeEquilibriumTest.cs
@@ -0,0 +1,24 @@
+﻿using Codility.Implementations.Lesson3;
+using Xunit;
+
+namespace Codility.Test.Lesson3
+{
+    public class TapeEquilibriumTest
+    {
+        [Fact]
+        public void Case01()
+        {
+            // Arrange
+            var algorithm = new TapeEquilibrium();
+            var A = new int[] {3, 1, 2, 4, 3 };
+
+            // Act
+            var result = algorithm.solution2(A);
+
+            // Assert
+            var expected = 1;
+
+            Assert.Equal(expected, result);
+        }
+    }
+}
